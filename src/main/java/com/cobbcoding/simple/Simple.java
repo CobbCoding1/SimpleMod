cb20ad35d17a49d9b9a0a29839b1bc34595c282b
CobbCoding1 <rircobb@gmail.com>
2021-02-12 16:29:45 -0500

    Set up outline of mod

---
+++ src/main/java/com/cobbcoding/simple/Simple.java
@@ -0,0 +1,11 @@
+package com.cobbcoding.simple;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Simple implements ModInitializer {
+
+    @Override
+    public void onInitialize(){
+
+    }
+}
