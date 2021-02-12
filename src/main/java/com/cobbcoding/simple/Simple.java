36b0a5b6ec6e3917667bb207d1b96b4a79157165
CobbCoding1 <rircobb@gmail.com>
2021-02-12 17:01:18 -0500

    Added copper ingot

--- src/main/java/com/cobbcoding/simple/Simple.java
+++ src/main/java/com/cobbcoding/simple/Simple.java
@@ -1,11 +1,14 @@
 package com.cobbcoding.simple;
 
+import com.cobbcoding.simple.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class Simple implements ModInitializer {
 
+    public static final String MOD_ID = "simple";
+
     @Override
     public void onInitialize(){
-
+        ModItems.registerItems();
     }
 }
